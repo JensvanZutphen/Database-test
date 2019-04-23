e375bd2034d05e334efba4b31ce15bd0265f47a2
JensvanZutphen <44496570+JensvanZutphen@users.noreply.github.com>
2019-04-23 11:21:08 +0200

    first test

---
+++ database test/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace database_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonplus = new System.Windows.Forms.Button();
+            this.buttonmin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(53, 194);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Tomaten";
+            // 
+            // buttonplus
+            // 
+            this.buttonplus.Location = new System.Drawing.Point(67, 165);
+            this.buttonplus.Name = "buttonplus";
+            this.buttonplus.Size = new System.Drawing.Size(75, 23);
+            this.buttonplus.TabIndex = 1;
+            this.buttonplus.Text = "+";
+            this.buttonplus.UseVisualStyleBackColor = true;
+            this.buttonplus.Click += new System.EventHandler(this.buttonplus_Click);
+            // 
+            // buttonmin
+            // 
+            this.buttonmin.Location = new System.Drawing.Point(67, 220);
+            this.buttonmin.Name = "buttonmin";
+            this.buttonmin.Size = new System.Drawing.Size(75, 23);
+            this.buttonmin.TabIndex = 2;
+            this.buttonmin.Text = "-";
+            this.buttonmin.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonmin);
+            this.Controls.Add(this.buttonplus);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonplus;
+        private System.Windows.Forms.Button buttonmin;
+    }
+}
+
---
+++ database test/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using MySql.Data.Common;
+
+namespace database_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void buttonplus_Click(object sender, EventArgs e)
+        {
+            var mysqlConnection = new MySqlConnection("user id=root;" +
+                                                      "password=;" +
+                                                      "server=localhost;" +
+                                                      "database=testwinkel;" +
+                                                      "connection timeout=5");
+            mysqlConnection.Open();
+            var leesaantaltomaat = "SELECT aantal from testwinkel WHERE id = 1";
+            var leesaantaltomaatcommand = new MySqlCommand(leesaantaltomaat, mysqlConnection);
+            var reader = leesaantaltomaatcommand.ExecuteScalar().ToString();
+            int reader1 = Convert.ToInt32(reader);
+            MessageBox.Show(reader);
+            mysqlConnection.Close();
+
+            System.Threading.Thread.Sleep(500);
+
+            mysqlConnection.Open();
+            var databasecommand = "UPDATE `testwinkel` SET aantal = " + (reader1+1) + " where id = 1";
+            var id = new MySqlCommand(databasecommand, mysqlConnection);
+            id.ExecuteNonQuery();
+            MessageBox.Show(databasecommand);
+            mysqlConnection.Close();
+        }
+    }
+}
